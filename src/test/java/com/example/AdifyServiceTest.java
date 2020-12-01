225eebf0777542cd4811b46a78f1179005779143
Martin Vinkel <mrvinkel@gmail.com>
2020-12-01 13:51:49 +0100

    Added component test

--- src/test/java/com/example/AdifyServiceTest.java
+++ src/test/java/com/example/AdifyServiceTest.java
@@ -4,6 +4,7 @@ import org.junit.Assert;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
+import java.io.IOException;
 import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -38,6 +39,35 @@ class AdifyServiceTest {
     Assert.assertEquals(4, bodyParts.length);
     Assert.assertEquals(sessionId, bodyParts[0]);
     Assert.assertEquals("advert", bodyParts[1]);
+  }
+
+  static class ExternalServiceStub implements ExternalService {
+
+    @Override
+    public String get(String arg) {
+      return "{\"product-name\":\"Paper\"}";
+    }
+  }
+
+  @Test
+  @Tag("fast")
+  public void testComponent() {
+    String sessionId = UUID.randomUUID().toString();
+
+    SenderSpy spy = new SenderSpy();
+    AdifyService a = new AdifyService(new Adify(new ExternalServiceStub()), sessionId + ",USER_ID,PRODUCT_ID", spy);
+    a.execute();
+
+    System.out.println(spy.event);
+    assertEquals("display", spy.event);
+
+    System.out.println(spy.body);
+    String[] bodyParts = spy.body.split(",");
+    Assert.assertEquals(4, bodyParts.length);
+    Assert.assertEquals(sessionId, bodyParts[0]);
+    Assert.assertEquals("advert", bodyParts[1]);
+    Assert.assertEquals("PRODUCT_ID", bodyParts[2]);
+    Assert.assertEquals("Paper", bodyParts[3]);
   }
 
 }
