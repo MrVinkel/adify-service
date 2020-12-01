9798348cad45b584df108fb8a3cb92f9d918ba0c
Martin Vinkel <mrvinkel@gmail.com>
2020-12-01 13:45:25 +0100

    Improve end to end test

--- src/test/java/com/example/AdifyServiceTest.java
+++ src/test/java/com/example/AdifyServiceTest.java
@@ -1,28 +1,43 @@
 package com.example;
 
+import org.junit.Assert;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
+
+import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class AdifyServiceTest {
 
-  class SenderSpy implements Subscription.Sender {
+  static class SenderSpy implements Subscription.Sender {
     String event;
+    String body;
 
     @Override
     public void send(String event, String body) {
       this.event = event;
+      this.body = body;
     }
   }
 
   @Test
   @Tag("slow")
-  void foo() {
+  void testEndToEnd() {
+    String sessionId = UUID.randomUUID().toString();
+
     SenderSpy spy = new SenderSpy();
-    AdifyService a = new AdifyService(new Adify(new HerokuGetRequest("adify")), "SESSION_ID,USER_ID,PRODUCT_ID", spy);
+    AdifyService a = new AdifyService(new Adify(new HerokuGetRequest("adify")), sessionId + ",USER_ID,PRODUCT_ID", spy);
     a.execute();
-    assertEquals("fixme", spy.event);
+
+    System.out.println(spy.event);
+    assertEquals("display", spy.event);
+
+    System.out.println(spy.body);
+    String[] bodyParts = spy.body.split(",");
+    Assert.assertEquals(4, bodyParts.length);
+    Assert.assertEquals(sessionId, bodyParts[0]);
+    Assert.assertEquals("advert", bodyParts[1]);
   }
 
 }
